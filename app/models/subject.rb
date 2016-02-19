ad3fc280c295ab4f312d2b8e4cd3e4310357b97e
kian1213 <chrono0424@gmail.com>
2016-02-19 16:47:04 +0800

    add admin users

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,7 @@ class Subject < ActiveRecord::Base
   has_many :courses, through: :course_subjects
   has_many :activities, as: :target
   has_many :tasks
+
+  accepts_nested_attributes_for :tasks, reject_if:
+    lambda {|a| a[:title].blank?}, allow_destroy: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  mount_uploader :avatar, AvatarUploader
   has_many :supervisor_courses
   has_many :courses, through: :supervisor_courses
   has_many :activities, as: :target
   has_many :user_courses
   has_many :user_subjects
   has_many :user_tasks
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
