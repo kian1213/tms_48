02906f22063ebac69c525ac3e778097aba65505d
kian1213 <chrono0424@gmail.com>
2016-02-19 12:38:43 +0800

    add devise login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get     "contact"     => "static_pages#contact", as: "contact"
   get     "help"        => "static_pages#help", as: "help"
 
+  namespace :admin do
+    root "users#index"
+    resources :users
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
